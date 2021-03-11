3d1cbc99be8615366cdd82319aa93eca49b7422f
YoofiBP <joseph.brown-pobee@ashesi.edu.gh>
2021-03-11 00:26:57 +0000

    Added Delete Mutations

--- index.ts
+++ index.ts
@@ -1,7 +1,73 @@
 import { GraphQLServer } from 'graphql-yoga';
-import {users, comments, posts} from './database';
 import { v4 as uuidv4 } from 'uuid';
-import {resourceExists} from "./utils";
+import { resourceExists} from "./utils";
+
+
+export let users = [
+    {
+        id: '1',
+        name: "Naa Ayorkor",
+        email: "naa@gmail.com",
+        age: 20,
+    },
+    {
+        id: '2',
+        name: "Yoofi",
+        email: "yoofi@gmail.com",
+        age: 24,
+    }
+]
+
+export let posts = [
+    {
+        id: "1",
+        title: "First Post",
+        body: "Post Body",
+        published: true,
+        author: "1"
+    },
+    {
+        id: "2",
+        title: "Second Post",
+        body: "Post Body",
+        published: true,
+        author: "2"
+    },
+    {
+        id: "3",
+        title: "Third Post",
+        body: "Post Body",
+        published: true,
+        author: "1"
+    }
+]
+
+export let comments = [
+    {
+        id: "1",
+        text: "First Comment",
+        author: "1",
+        post: "1"
+    },
+    {
+        id: "2",
+        text: "Second Comment",
+        author: "1",
+        post: "2"
+    },
+    {
+        id: "3",
+        text: "Third Comment",
+        author: "2",
+        post: "3"
+    },
+    {
+        id: "4",
+        text: "Fourth Comment",
+        author: "2",
+        post: "2"
+    }
+]
 
 //Type Definitions - Application Schema
 const typeDefs = `
@@ -22,9 +88,33 @@ const typeDefs = `
     }
     
     type Mutation {
-        createUser(name: String!, email: String!, age: Int): User!
-        createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
-        createComment(text: String!, author: ID!, post: ID!): Comment!
+        createUser(data: CreateUserInput): User!
+        deleteUser(userId: ID!): User
+        
+        createPost(data: CreatePostInput): Post!
+        deletePost(postId: ID!): Post
+        
+        createComment(data: CreateCommentInput): Comment!
+        deleteComment(commentId: ID!):Comment!
+    }
+    
+    input CreateUserInput {
+        name: String!
+        email: String!
+        age: Int
+    }
+    
+    input CreatePostInput {
+        title: String!
+        body: String!
+        published: Boolean!
+        author: ID!
+    }
+    
+    input CreateCommentInput {
+        text: String!
+        author: ID!
+        post: ID!
     }
     
     type User {
@@ -121,8 +211,8 @@ const resolvers = {
 
     Mutation: {
         createUser(parent, args){
-            if(args){
-                const {name, email, age} = args;
+            if(args.data){
+                const {name, email, age} = args.data;
                 const emailTaken = resourceExists(users, email, "email");
                 if(emailTaken){
                     throw new Error('Email Taken');
@@ -140,9 +230,60 @@ const resolvers = {
             }
         },
 
-        createPost(parent, args){
+        deleteUser(parent,args) {
+          if(args){
+              const {userId} = args;
+              const userIndex = users.findIndex(user => user.id === userId);
+
+              if(userIndex === -1){
+                  throw new Error('User not found');
+              }
+
+              const deletedUser = users.splice(userIndex, 1)[0];
+
+              //remove all posts relating to author
+              posts = posts.filter(post => {
+                  const toBeRemoved =  post.author === userId
+
+                  if(toBeRemoved){
+                      //remove all comments relating to post
+                    comments = comments.filter(comment => {
+                        return comment.post !== post.id
+                    })
+                  }
+                  return !toBeRemoved;
+              });
+
+              //remove all comments relating to author
+              comments = comments.filter(comment => {
+                  return comment.author !== userId
+              })
+
+              return deletedUser;
+          }
+        },
+
+        deletePost(parent, args){
             if(args){
-                const { author } = args;
+                const {postId} = args;
+                const post = posts.find(post => post.id === postId);
+
+                if(!post){
+                    throw new Error('Post does not exist')
+                }
+                //remove posts
+                posts = posts.filter(post => post.id !== postId);
+
+                //remove related comments
+                comments = comments.filter(comment => comment.post !== postId)
+
+                return post
+            }
+        },
+
+        createPost(parent, args){
+            if(args.data){
+                const { author } = args.data;
                 const validAuthor = resourceExists(users, author, "id");
                 if(!validAuthor){
                     throw new Error('Author does not exist')
@@ -150,7 +291,7 @@ const resolvers = {
                 const id = uuidv4();
                 const post = {
                     id,
-                   ...args
+                   ...args.data
                 }
 
                 posts.push(post)
@@ -160,7 +301,7 @@ const resolvers = {
 
         createComment(parent, args){
             if(args){
-                const {author, post} = args;
+                const {author, post} = args.data;
                 const user = resourceExists(users, author, "id");
                 if(!user){
                     throw new Error('User does not exist')
@@ -171,12 +312,28 @@ const resolvers = {
                 }
                 const comment = {
                     id: uuidv4(),
-                    ...args
+                    ...args.data
                 }
 
                 comments.push(comment);
 
                 return comment
+            }
+        },
+
+        deleteComment(parent, args){
+            if(args){
+                const {commentId} = args;
+
+                const comment = comments.find(comment => comment.id === commentId);
+
+                if(!comment){
+                    throw new Error('Comment does not exist')
+                }
+
+                comments = comments.filter(comment => comment.id !== commentId);
+
+                return comment;
             }
         }
     },
